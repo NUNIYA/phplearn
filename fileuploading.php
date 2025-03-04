0c036a78cdb00fba74555d2e89730742803818f4
NUNIYA <nuniyat.g@gmail.com>
2025-03-04 21:07:25 +0300

    done with file uploading

--- fileuploading.php
+++ fileuploading.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
         $file_name = $_FILES['upload']['name'];
         $file_size = $_FILES['upload']['size'];
         $file_tmp = $_FILES['upload']['tmp_name'];
-        $target_dir = "uploads/{file_name}";
+        $target_dir = "uploads/" . $file_name;
         //get file extension pf uploaded file and check if there extension matches with the allowed once
         $file_ext = explode('.', $file_name); //explode function creates and array from string so in this case it will split it using . $file_name wich will return the uploaded file name for e.g. pp.png so the spliting is done on pp and png and stored as an array
         $file_ext = strtolower(end($file_ext)); //now what this does is from the array on the above it will take the end which will be the uploaded file end extension for eg if the uploaded file is pp.png first it split it in to pp and png and the ned of the array will be png  which this line of code extracts   
